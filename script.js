df6a9deb68c4b3aacd9a90cc5037e82f15733565
Anndersonolive <anndersonolive@gmail.com>
2022-05-03 10:37:09 -0300

    link_js_no_arquivo

---
+++ script.js
@@ -0,0 +1,7 @@
+function zap(){
+    window.open("https://www.youtube.com/watch?v=c-kUoHk9ggw&ab_channel=DevMedia")
+}
+
+function comp(){
+    window.open("https://www.youtube.com/watch?v=c-kUoHk9ggw&ab_channel=DevMedia")
+}
